a03a6f7d416d926c38db429f3e44de830235ea1b
ilyafomin <ilya.fomin@erdw.ethz.ch>
2018-01-12 18:42:42 +0100

    Create rollingaverage.sh

---
+++ rollingaverage.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# This script takes a tab delimited table of numbers and computes rolling averages
+# (c) Ilya Fomin , 2018
+
+#Averaging window size
+window=10
+
+# check and report filenames to be processed
+if [ $# -eq 0 ]
+then
+  echo "Pass an input file name as an argument."
+  exit
+fi
+filename=$1
+echo "Input file: $filename"
+outputfn=$2
+if [ "$outputfn" = "" ]
+then
+  echo "No output file will be written; all the output goes to the console"
+else
+  echo "Output file: $outputfn"
+  # flush it contents
+  echo "" > $outputfn
+fi
+
+# learn and report number of elements in each row
+argnum=$(head -n 1 $filename | wc -w)
+echo "Found $argnum data entries to be averaged"
+
+# report how many folds do we shrink the dataset
+echo "Rolling average window is $window"
+
+# init arrays and make sure they are zeroed
+for (( j=1 ; j<=$argnum ; j=j+1 ))
+do
+  total[$j]=0.0;
+done
+
+j=1 #counter of elements
+k=0 #counter of rows
+
+# it returns consequentially each element line by line
+for i in $( awk '{ print }' tempdata.txt )
+do
+  # it adds the current value to a specific array term and increases index
+  total[$j]=$(echo "scale=5; ${total[$j]} + $i" | bc)
+  (( j++ ))
+  #is row finished?
+  if [ "$j" -gt "$argnum" ]
+  then
+    #we are at the end of a row => flush j and increase k
+    j=1
+    (( k++ ))
+  fi
+  #should we collect and output stats?
+  if [ "$k" -eq "$window" ]
+  then
+    #we are at the end of a row => flush k
+    k=0
+    output=""
+    for (( j=1 ; j<=$argnum ; j++ ))
+    do
+      #get an average
+      value=$(echo "scale=5; ${total[$j]}/$window" | bc | sed 's/^\./0./')
+      #add to output line; add comma only if we are not at the end
+      output+="$value"
+      if [ "$j" -lt "$argnum" ] ; then output+="," ; fi
+      #zero the sum
+      total[$j]=0.0;
+    done
+    j=1 # to rewind the elements in the next row
+
+    #output
+    if [ "$outputfn" != "" ]
+    then
+      echo "$output" >> $outputfn
+    else
+      echo "$output"
+    fi
+#    exit
+  fi
+done
